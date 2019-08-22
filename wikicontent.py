9129f07d307abd05e933e5262a340148c409ba90
Laurence <laurence+github@entek.org.uk>
2019-08-22 12:33:44 +0100

    Table caption support and full table test (#52)
    * Added support for table captions.
    
    * Changed to convert captions to bold text and add tests.
    
    Thought bold less likely to be controversial than converting to a
    specific level heading.

--- wikicontent.py
+++ wikicontent.py
@@ -275,6 +275,19 @@ def convert(node, context, trailing_newline):
     # anything else is inline term
     return "$" + node.math + "$"
 
+
+@visitor.when(Caption)
+def convert(node, context, trailing_newline):
+	"""
+	Convert table captions to bold paragraph preceeding the table.
+	
+	Because we ignore the <table> tags when converting to dokuwiki,
+	we can get away with simply converting to bold text without
+	worrying about it being inside <table> (which <caption> should be)
+	in the rendered HTML.
+	"""
+	return "** %s **\n" % convert_children(node, context)
+
 # catchall for Node, which is the parent class of everything above
 @visitor.when(Node)
 def convert(node, context, trailing_newline):
